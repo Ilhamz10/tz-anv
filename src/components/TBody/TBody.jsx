6a0e61c74c1dc8ae867f5288f5e18f227c1be4e8
Ilhamz10 <ilhamzeynalov65@gmail.com>
2024-01-25 12:40:01 +0400

    change drag and drop animation

--- src/components/TBody/TBody.jsx
+++ src/components/TBody/TBody.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import styles from './TBody.module.css';
 import TRow from './TRow/TRow';
 import { usersActions } from '../../store/users-slicer';
@@ -8,23 +8,30 @@ import { useDispatch } from 'react-redux';
 const TBody = ({ data }) => {
 	const dispatch = useDispatch();
 	const [currentUser, setCurrentUser] = useState(null);
+	const currentRow = useRef(null);
 
 	function dragStartHandler(e, user) {
+		e.target.style.opacity = '0';
+		e.target.style.position = 'absolute';
+		e.target.style.zIndex = '0';
+		currentRow.current = e.target;
 		setCurrentUser(user);
 	}
 
 	function dragEndHandler(e) {
-		e.target.parentElement.style.borderBottom = 'none';
+		e.target.parentElement.style.borderBottom = '3px solid transparent';
 	}
 
 	function dragOverHandler(e) {
 		e.preventDefault();
-		e.target.parentElement.style.borderBottom = '3px solid green';
+		e.target.parentElement.style.borderBottom = `60px solid #d6deff`;
 	}
 
 	function dropHandler(e, user) {
 		e.preventDefault();
-		e.target.parentElement.style.borderBottom = 'none';
+		e.target.parentElement.style.borderBottom = '3px solid transparent';
+		currentRow.current.style.opacity = '1';
+		currentRow.current.style.position = 'static';
 		dispatch(
 			usersActions.dragAndDrop({
 				currentId: currentUser.id,
